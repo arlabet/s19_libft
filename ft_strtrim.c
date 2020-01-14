aefe43af608d4d8585e512a6dfc8f5e66e9f9dcf
Najima Sahloumi <nsahloum@w-r7-p5.s19.be>
2020-01-14 15:49:36 +0100

    trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,45 +6,53 @@
 /*   By: nsahloum <nsahloum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 19:23:23 by nsahloum          #+#    #+#             */
-/*   Updated: 2020/01/13 21:08:23 by nsahloum         ###   ########.fr       */
+/*   Updated: 2020/01/14 15:45:48 by nsahloum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stdio.h"
 
-int		ft_inset(char const *set, char c)
+static int ft_isinset(char const *set, char c)
 {
 	size_t i;
 
 	i = 0;
-	while (set[i])
+	while (s1[i])
 	{
-		if (c == set[i])
+		if (s1[i] == c)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-size_t	ft_lencut(char const *s1, char const *set)
+static int ft_start(char const *s1, char const *set)
 {
-	size_t len;
 	size_t start;
-	size_t i;
 
-	i = 0;
 	start = 0;
-	len = ft_strlen(s1);
-	while (s1[i])
-	{
-		while (ft_inset(set, s1[i]))
-			start++;
-		i++;
-	}
-	while (len > 0)
-	{
-		while (ft_inset(set, s1[len - 1]))
-			len--;
-	}
-	return (len - start);
+	while (s1[i] && ft_inset(set, s1[i]))
+		start++;
+}
+
+static int ft_end(char const *s1, char const *set)
+{
+	size_t size;
+	
+	size = ft_strlen(s1);
+	while ()
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	
+}
+
+int main (void)
+{
+	char const s1[80] = "a .b .c .d .e .f";
+	char const set[80] = " .";
+
+	printf("%s", ft_strtrim(s1, set));
 }
