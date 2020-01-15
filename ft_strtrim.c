b8add82b43c3d1d674694666ad677041b149b79b
arlabet <sahloumi.najima@gmail.com>
2020-01-15 01:05:03 +0100

    strtrim enfin

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,53 +6,45 @@
 /*   By: nsahloum <nsahloum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 19:23:23 by nsahloum          #+#    #+#             */
-/*   Updated: 2020/01/14 15:45:48 by nsahloum         ###   ########.fr       */
+/*   Updated: 2020/01/14 21:13:40 by nsahloum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
 
-static int ft_isinset(char const *set, char c)
+static int	ft_isinset(char const *set, char c)
 {
 	size_t i;
 
 	i = 0;
-	while (s1[i])
+	while (set[i])
 	{
-		if (s1[i] == c)
+		if (set[i] == c)
+		{
 			return (1);
+		}
 		i++;
 	}
 	return (0);
 }
-
-static int ft_start(char const *s1, char const *set)
+char		*ft_strtrim(char const *s1, char const *set)
 {
-	size_t start;
+	size_t	start;
+	size_t	end;
+	char	*res;
 
+	if (!set || !s1)
+		return (0);
+	end = ft_strlen(s1);
 	start = 0;
-	while (s1[i] && ft_inset(set, s1[i]))
+	while (ft_isinset(set, s1[start]))
 		start++;
-}
-
-static int ft_end(char const *s1, char const *set)
-{
-	size_t size;
-	
-	size = ft_strlen(s1);
-	while ()
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	
-}
-
-int main (void)
-{
-	char const s1[80] = "a .b .c .d .e .f";
-	char const set[80] = " .";
-
-	printf("%s", ft_strtrim(s1, set));
+	while (ft_isinset(set, s1[end - 1]))
+		end--;
+	if (end <= start)
+		end = start;
+	if (!(res = malloc(sizeof(char) * (end - start + 1))))
+		return (0);
+	res = ft_substr(s1, start, (end - start));
+	return (res);
 }
